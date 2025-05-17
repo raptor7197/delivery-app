147ffb279bad6507406bb07a826368910fb55552
raptor7197 <vamsikrishna75836@gmail.com>
2025-05-18 00:50:39 +0530

    28:38

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,9 +1,11 @@
 import React from 'react'
+import Navbar from './components/Navbar'
 
 const App = () => {
   return (
     <div>
-      skibidi sigma
+      skibidi sigma whooo 
+      <Navbar/>
     </div>
   )
 }
--- client/src/context/AppContext.jsx
+++ client/src/context/AppContext.jsx
@@ -4,10 +4,15 @@ export const AppProvider = ({ children }) => {
    const navigate = useNavigate();
    const [user, setUser] = useState(null);
     const [seller, setIsseller] = useState(false);
+    const value = {navigate , user, setUser, seller, setIsseller};
+    const [isLoading, setIsLoading] = useState(false);
     
     return (
         <AppContext.Provider>
         {children}
         </AppContext.Provider>
     );
+}
+export const useAppContext = () => {
+    return useContext(AppContext);
 }
