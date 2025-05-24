b7bc571a146cb3e2b1cfd8a3ef19eaff188c21c9
raptor7197 <vamsikrishna75836@gmail.com>
2025-05-25 00:23:46 +0530

    1:18:27

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,13 +1,24 @@
-import React from 'react'
-import Navbar from './components/Navbar.jsx'
+import React from "react";
+import Navbar from "./components/Navbar.jsx";
+import { BrowserRouter, Routes, Route, useLocation } from "react-router-dom";
+import Home from "./pages/Home";
 
 const App = () => {
 
-  return (
-   <div>
-      <Navbar/>
-   </div>
-  )
-}
+  const isSellerPath = useLocation().pathname.includes("/seller");
 
-export default App
+  return (
+    <div>
+      {isSellerPath?null:<Navbar />}
+      
+      <div className= {`${isSellerPath} ? "" : "px-6 md:px-16 lg:px-24 xl:px-32"}`}>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          
+        </Routes>
+      </div>
+    </div>
+  );
+};
+
+export default App;
--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -15,7 +15,7 @@ const Navbar = () => {
   return (
     <div>
       <nav className="flex items-center justify-between px-6 md:px-16 lg:px-24 xl:px-32 py-4 border-b border-gray-300 bg-white relative transition-all">
-        <NavLink href="/">
+        <NavLink to="/" onClick={() => setOpen(false)}>
           <img className="h-9" src={assets.logo} alt="logo" />
         </NavLink>
 
@@ -32,46 +32,13 @@ const Navbar = () => {
               type="text"
               placeholder="Search products"
             />
-            <svg
-              width="16"
-              height="16"
-              viewBox="0 0 16 16"
-              fill="none"
-              xmlns="http://www.w3.org/2000/svg"
-            >
-              <path
-                d="M10.836 10.615 15 14.695"
-                stroke="#7A7B7D"
-                stroke-width="1.2"
-                stroke-linecap="round"
-                stroke-linejoin="round"
-              />
-              <path
-                clip-rule="evenodd"
-                d="M9.141 11.738c2.729-1.136 4.001-4.224 2.841-6.898S7.67.921 4.942 2.057C2.211 3.193.94 6.281 2.1 8.955s4.312 3.92 7.041 2.783"
-                stroke="#7A7B7D"
-                stroke-width="1.2"
-                stroke-linecap="round"
-                stroke-linejoin="round"
-              />
-            </svg>
+            
+            <img src={assets.search_icon} alt="search" />
           </div>
 
-          <div className="relative cursor-pointer">
-            <svg
-              width="18"
-              height="18"
-              viewBox="0 0 14 14"
-              fill="none"
-              xmlns="http://www.w3.org/2000/svg"
-            >
-              <path
-                d="M.583.583h2.333l1.564 7.81a1.17 1.17 0 0 0 1.166.94h5.67a1.17 1.17 0 0 0 1.167-.94l.933-4.893H3.5m2.333 8.75a.583.583 0 1 1-1.167 0 .583.583 0 0 1 1.167 0m6.417 0a.583.583 0 1 1-1.167 0 .583.583 0 0 1 1.167 0"
-                stroke="#615fff"
-                stroke-linecap="round"
-                stroke-linejoin="round"
-              />
-            </svg>
+          <div onClick={()=> navigate("cart")} className="relative cursor-pointer">
+           
+            <img src={assets.cart_icon} alt="cart" />
             <button className="absolute -top-2 -right-3 text-xs text-white bg-teal-500 w-[18px] h-[18px] rounded-full">
               3
             </button>
@@ -116,7 +83,7 @@ const Navbar = () => {
           className="sm:hidden"
         >
           <img src={assets.menu_icon} alt="menu" />
-          true
+          
         </button>
 
         {/* mobile  */}
